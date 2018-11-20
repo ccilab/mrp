1f1639158ad7c8eb55ae6bfec42cd6699fc6d137
zhangchar <zhang_charlie@hotmail.com>
2018-11-21 00:49:55 +1100

    refactor to remove evnet handler function definition

--- src/hello-world/src/cci_components/cci_part/AddGreeter.js
+++ src/hello-world/src/cci_components/cci_part/AddGreeter.js
@@ -5,37 +5,29 @@ class AddGreeter extends Component {
     state = { greetingName: "" ,
               progressValue: ""};
 
-    handleUpdate = (event)=>{
-      this.setState({ greetingName: event.target.value });
-    };
-
-    handleProgressUpdate = (event)=>{
-      this.setState({progressValue: event.target.value})
-    };
-    addGreeting = () =>{
-      this.props.addGreeting(this.state.greetingName, this.state.progressValue);
-      this.setState({ greetingName: "", progressValue:"" });
-    };
-  
-  
-
-
     render = () => {
       return (
         <div className="AddGreeter">
           <input
             type="text"
-            onChange={this.handleUpdate}
+            onChange={(event)=>{
+              this.setState({ greetingName: event.target.value })}}
             value={this.state.greetingName}>
             </input>
           
           <input 
             type="text"
-            onChange={this.handleProgressUpdate}
+            onChange={(event)=>{
+              this.setState({progressValue: event.target.value})}}
             value={this.state.progressValue} 
           />
           &nbsp;&nbsp;
-          <button onClick={this.addGreeting}>Add</button>
+          <button onClick={ () =>{
+            this.props.addGreeting(this.state.greetingName, this.state.progressValue);
+            this.setState({ greetingName: "", progressValue:"" });
+            } }>
+            Add
+            </button>
         </div>
       );
     };
