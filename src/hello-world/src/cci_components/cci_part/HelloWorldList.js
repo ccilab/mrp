3e77c19a15d1259c32969415d5b1b829a29fa909
ccilab <zhang_charlie@hotmail.com>
2018-11-23 14:16:20 +1100

    handle expending or collapsing child components

--- src/hello-world/src/cci_components/cci_part/HelloWorldList.js
+++ src/hello-world/src/cci_components/cci_part/HelloWorldList.js
@@ -4,22 +4,40 @@ import "./HelloWorldList.css";
 import CCiLabComponent from "./HelloWorld";
 import AddGreeter from "./AddGreeter";
 
+//image file name should be created the same as component name, so we can create imgName dynamically
+//image type is extracted from uploaded image file
+//table includes assembly and paint process
+const components = [ {id: 00, name: 'table', parentIds:[], childIds:[01,02,03,04,05,06], imgType: 'png', status: 'no_issue', progressPercent:0, showMyself:false},
+                     {id: 01, name:'top', parentIds:[00], childIds:[05,06], imgType:'jpg', status: 'warning', progressPercent: 40, showMyself: false},
+                     {id: 02, name:'leg', parentIds:[00], childIds:[05,06], imgType:'jpg', status: 'alarm', progressPercent: 10, showMyself: false},
+                     {id: 03, name:'upper_beam', parentIds:[00], childIds:[05,06], imgType:'jpg', status: 'no_issue', progressPercent: 50, showMyself: false},
+                     {id: 04, name:'low_beam', parentIds:[00], childIds:[05,06], imgType:'jpg', status: 'warning', progressPercent: 20, showMyself: false},
+                     {id: 05, name:'nail', parentIds:[00], childIds:[], imgType:'', status: 'no_issue', progressPercent: 100, showMyself: false},
+                     {id: 06, name:'glue', parentIds:[00], childIds:[], imgType:'', status: 'no_issue', progressPercent: 100, showMyself: false},
+                    ]
 
 class HelloWorldList extends Component {
-    state = { greetings: [ {progressPercent: 40, status: "warning", imgName: "edit_32.png", parentIds: [], siblingIds: [] }, 
-                           {progressPercent: 20, status: "no_issue", imgName: "edit.svg", parentIds: ['p3', 'p4'], siblingIds: ['s21','s22'] }, 
-                           {progressPercent: 90, status: "alarm", imgName: "table.png", parentIds: ['p5', 'p6'], siblingIds: ['s31','s32','s33'] }]}
-    
+    state = { greetings: [...components] };
   
     addGreeting = (newName, progressValue) =>{
       this.setState({ greetings: [...this.state.greetings, 
-                                  {progressPercent: progressValue, status: "alarm", imgName: newName, parentIds: ['p5', 'p6'], siblingIds: ['s5','s6'] }] });
-    }
+                                  {id: components.length + 1, name: newName, parentIds:[00], childIds:[], imgType:'', status: "alarm", progressPercent: progressValue,  showMyself: true}] });
+    };
+
+    //need to update showMyself to true after button is clicked to expend
+    //need to update showMyself to false after button is clicked to collaps
+    isShowChildren= ( component )=>{
+      if( component.parentIds.length === 0)
+        component.showMyself = true;
+
+      if( component.showMyself === true )
+        return <CCiLabComponent {...cComponent} removeGreeting={this.removeGreeting} /> ;
+    };
 
     renderGreetings = () => {
-      return this.state.greetings.map(cComponent => (
-        <CCiLabComponent {...cComponent} removeGreeting={this.removeGreeting} />
-      ));
+      return this.state.greetings.map( cComponent => 
+        ( <CCiLabComponent {...cComponent} removeGreeting={this.removeGreeting} /> ),
+        key=cComponent.id);
     };
 
   
