eb3a9e7d12d6c8e66a486d208e8cd2716c437436
ccilab <zhang_charlie@hotmail.com>
2018-11-30 14:38:30 +1100

    using childKeyIds or childIds to show children

--- src/hello-world/src/cci_components/cci_part/HelloWorldList.js
+++ src/hello-world/src/cci_components/cci_part/HelloWorldList.js
@@ -7,13 +7,13 @@ import AddGreeter from "./AddGreeter";
 //image file name should be created the same as component name, so we can create imgName dynamically
 //image type is extracted from uploaded image file
 //table includes assembly and paint process
-const components = [ {key: 0, id: 0, name: 'table', parentIds:[], childIds:[1,2,3,4,5,6], childKeyIds:[], imgType: 'png', status: 'no_issue', progressPercent:0, showMyself:false, toBeExpend: true, insertCnt: 0},
-                     {key: 1,id: 1, name:'top', parentIds:[0], childIds:[5,6], childKeyIds:[],imgType:'jpg', status: 'warning', progressPercent: 40, showMyself: false, toBeExpend: true, insertCnt: 0},
-                     {key: 2,id: 2, name:'leg', parentIds:[0], childIds:[5,6], childKeyIds:[],imgType:'jpg', status: 'alarm', progressPercent: 10, showMyself: false, toBeExpend: true, insertCnt: 0},
-                     {key: 3,id: 3, name:'upper_beam', parentIds:[0], childIds:[5,6], childKeyIds:[], imgType:'jpg', status: 'no_issue', progressPercent: 50, showMyself: false, toBeExpend: true, insertCnt: 0},
-                     {key: 4,id: 4, name:'low_beam', parentIds:[0], childIds:[5,6], childKeyIds:[], imgType:'jpg', status: 'warning', progressPercent: 20, showMyself: false, toBeExpend: true, insertCnt: 0},
-                     {key: 5,id: 5, name:'nail', parentIds:[0,1,2,3,4], childIds:[], childKeyIds:[], imgType:'', status: 'no_issue', progressPercent: 10, showMyself: false, toBeExpend: false, insertCnt: 0},
-                     {key: 6,id: 6, name:'glue', parentIds:[0,1,2,3,4], childIds:[], childKeyIds:[],imgType:'', status: 'no_issue', progressPercent: 10, showMyself: false, toBeExpend: false, insertCnt: 0},
+const components = [ {id: 0, name: 'table', parentIds:[], childIds:[1,2,3,4,5,6],  imgType: 'png', status: 'no_issue', progressPercent:0, key: 0,childKeyIds:[], showMyself:false, toBeExpend: true, insertCnt: 0},
+                     {id: 1, name:'top', parentIds:[0], childIds:[5,6], imgType:'jpg', status: 'warning', progressPercent: 40, key: 1,childKeyIds:[], showMyself: false, toBeExpend: true, insertCnt: 0},
+                     {id: 2, name:'leg', parentIds:[0], childIds:[5,6], imgType:'jpg', status: 'alarm', progressPercent: 10, key: 2, childKeyIds:[],showMyself: false, toBeExpend: true, insertCnt: 0},
+                     {id: 3, name:'upper_beam', parentIds:[0], childIds:[5,6],  imgType:'jpg', status: 'no_issue', progressPercent: 50,key: 3,childKeyIds:[], showMyself: false, toBeExpend: true, insertCnt: 0},
+                     {id: 4, name:'low_beam', parentIds:[0], childIds:[5,6],  imgType:'jpg', status: 'warning', progressPercent: 20,key: 4,childKeyIds:[], showMyself: false, toBeExpend: true, insertCnt: 0},
+                     {id: 5, name:'nail', parentIds:[0,1,2,3,4], childIds:[],  imgType:'', status: 'no_issue', progressPercent: 10,key: 5, childKeyIds:[],showMyself: false, toBeExpend: false, insertCnt: 0},
+                     {id: 6, name:'glue', parentIds:[0,1,2,3,4], childIds:[], imgType:'', status: 'no_issue', progressPercent: 10,key: 6,childKeyIds:[], showMyself: false, toBeExpend: false, insertCnt: 0},
                     ]
 
 class HelloWorldList extends Component {
@@ -31,6 +31,7 @@ class HelloWorldList extends Component {
       this.setState({ greetings: filteredGreetings });
     };
 
+    //based on selected component and its show Status to show or hide its children
     showChildren = ( showChildrenComponent, showStatus ) =>{
           let updateAllComponents =this.state.greetings;
           let showChildrenComponentId = showChildrenComponent.id;
@@ -42,12 +43,16 @@ class HelloWorldList extends Component {
           // looping through selected component's child component list
             if( showStatus === true ) 
             {  
-              for( let idxChildId = 0; idxChildId < showChildrenComponent.childIds.length; idxChildId++) 
+              let displayChildList = showChildrenComponent.childKeyIds.length ? showChildrenComponent.childKeyIds : showChildrenComponent.childIds;
+              // length of childIds and childKeyIds should be same, #todo: need to add check here
+              for( let idxChildId = 0; idxChildId < displayChildList.length; idxChildId++) 
               {
                 // looping through entire component list to find the component included inside child component list
                 for( let idxComponent = 0;  idxComponent < updateAllComponents.length; idxComponent++ ) 
                 {
                   // find the component that is the child component, and update the show status of this component
+                  // if childKeyIds.length !===0 use childKeyIds to find child component and show it,
+                  // if childKeyIds.length ===0, use childIds to find child component, show it, create childKey and insert this new component to component lists
                   if( updateAllComponents[idxComponent].id === showChildrenComponent.childIds[idxChildId] ) 
                   {
                       updateAllComponents[idxComponent].showMyself = showStatus;
@@ -62,6 +67,7 @@ class HelloWorldList extends Component {
                           // create an new object to update so the original object won't be updated 
                           let cloneComponent = Object.assign({}, childrenComponentList[0]);
                           cloneComponent.key = updateAllComponents.length+1;
+                          showChildrenComponent.childKeyIds.push( cloneComponent.key );
                           let idxInsertAt = updateAllComponents.findIndex(showChildrenComponent=>{return showChildrenComponent.id === showChildrenComponentId});
                           // insert child component under direct parent
                           updateAllComponents.splice( idxInsertAt+1,0,cloneComponent);
