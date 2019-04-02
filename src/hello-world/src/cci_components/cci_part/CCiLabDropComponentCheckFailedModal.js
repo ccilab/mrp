1ce57c904ddb18dbae140bc401aa8d50fdb01ba2
zhangchar <zhang_charlie@hotmail.com>
2019-04-03 00:45:04 +1100

    remove tool-tip-controller

--- src/hello-world/src/cci_components/cci_part/CCiLabDropComponentCheckFailedModal.js
+++ src/hello-world/src/cci_components/cci_part/CCiLabDropComponentCheckFailedModal.js
@@ -58,35 +58,3 @@ function DropComponentWarningModal(props) {
 }
 
 export default DropComponentWarningModal;
-
-// const dropDomponentWarningModalBase = (
-//     <div className='modal fade'>
-//         <div className='modal-dialog'>
-//             <div className='modal-content'>
-//                 {/* Modal Header */}
-//                 <div className='modal-header'>
-//                     <h4 className='modal-title text-danger'>Warning:</h4>
-//                     <button type='button' className='close' data-dismiss='modal'>&time;</button>
-//                 </div>
-
-//                 {/* Modal body */}
-//                 <div className="modal-body">
-//                     Target component has same item as its child component!
-//                 </div>
-
-//                 {/* Modal footer */}
-//                 <div className="modal-footer">
-//                 <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
-//                 </div>
-//                 </div>
-//         </div>
-        
-//     </div>
-// );
-
-// // #drop-component-check-failed-modal is id defined in index.html
-// const DropComponentCheckFailedModal =(props)=>{
-//     return ReactDOM.createPortal( dropDomponentWarningModalBase, document.querySelector('#drop-component-check-failed-modal') );
-// }
-
-// export default DropComponentCheckFailedModal;
