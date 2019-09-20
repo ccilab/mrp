19010f1fe755d2d114deb7ed6881e1caac740a82
Charlie Zhang <Charlie.Zhang@autodesk.com>
2019-09-20 14:16:26 +1000

    creating MPS table

---
+++ src/hello-world/src/cci_components/cci_part/CCiLabMPSTable.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+import { useTranslation } from 'react-i18next';
+
+import styles from "./../../dist/css/ccilab-component-list.css"
+
+
+export const MPSTable(props)=>{
+    
+}
