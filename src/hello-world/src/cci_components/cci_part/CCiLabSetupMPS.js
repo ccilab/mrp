529ab72b7f7f67479a95092dedcbda055e91c1f7
Charlie Zhang <Charlie.Zhang@autodesk.com>
2020-04-29 15:36:05 +1000

    update MPS design

--- src/hello-world/src/cci_components/cci_part/CCiLabSetupMPS.js
+++ src/hello-world/src/cci_components/cci_part/CCiLabSetupMPS.js
@@ -26,16 +26,27 @@ const _initialMPS=(component)=>{
 
   mps.partName = component.businessLogic.name;
   mps.partNumber = component.bom.core.partNumber;
-  mps.startDate = null;
-  mps.productionDueDate = null;
-  mps.leadTime = null;
-  mps.setupCost = null;
-  mps.shiftMode = null;
-  mps.planningHorizonCount = 1;
-  mps.inventoryOnHand = 0;
-  mps.scrapRate = 0;
-  mps.grossDemand = 0;
-  mps.netDemand = 0;
+  mps.startDate = component.operation.startDate;
+
+  //https://www.danvega.dev/blog/2019/03/14/find-max-array-objects-javascript/
+  mps.productionDueDate = component.pdp.demandAndEndDateArray.reduce( ( lastDate, item )=> ( 
+                                                                component.pdp.demandAndEndDateArray[0].completeDate !== null && 
+                                                                item.completeDate !== null && 
+                                                                item.completeDate > lastDate ? item.completeDate : lastDate ),  
+                                                                component.pdp.demandAndEndDateArray[0].completeDate );
+     
+
+  mps.leadTime = component.irf.leadTime.value + component.irf.leadTime.timeUnit;
+  mps.setupCost = component.operation.setupCost;
+  mps.shiftMode = component.operation.shiftType; //need to localize from 'normal-hours' or 'shift'
+  mps.planningHorizonCount = component.pdp.demandAndEndDateArray.length;
+  mps.inventoryOnHand = component.irf.inventoryOnHand;
+  mps.scrapRate = component.operation.scrapRate;
+  mps.grossDemand = component.pdp.demandAndEndDateArray.reduce( ( grossDemand, item )   => ( 
+                                                                  component.pdp.demandAndEndDateArray[0].requiredQuantity !== null &&
+                                                                  item.requiredQuantity !== null ? grossDemand +=item.requiredQuantity : grossDemand ), 
+                                                                  component.pdp.demandAndEndDateArray[0].requiredQuantity );;
+  mps.netDemand = mps.grossDemand !== null ? mps.grossDemand - mps.inventoryOnHand : null;
   mps.lotMethod = null;
   mps.lotSize = 0;
   mps.timeBucket = 0;
